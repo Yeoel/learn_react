6ff492e4932ecba2996a685367b502fe72789f6b
Yeoel <136804491+Yeoel@users.noreply.github.com>
2024-03-11 23:24:38 +0700

    Revert "Revert "feat: learning react completed""
    This reverts commit 3ab9959c310883a0d09d975b15801f47788dcdb7.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
@@ -18,12 +19,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
+    noStore();
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -45,6 +46,8 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+    noStore();
+
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -74,6 +77,8 @@ export async function fetchCardData() {
     const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+
+    noStore();
 
     return {
       numberOfCustomers,
@@ -137,6 +142,7 @@ export async function fetchInvoicesPages(query: string) {
   `;
 
     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    noStore();
     return totalPages;
   } catch (error) {
     console.error('Database Error:', error);
@@ -162,6 +168,9 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    noStore();
+
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
@@ -180,6 +189,8 @@ export async function fetchCustomers() {
     `;
 
     const customers = data.rows;
+    noStore();
+
     return customers;
   } catch (err) {
     console.error('Database Error:', err);
@@ -212,6 +223,7 @@ export async function fetchFilteredCustomers(query: string) {
       total_pending: formatCurrency(customer.total_pending),
       total_paid: formatCurrency(customer.total_paid),
     }));
+    noStore();
 
     return customers;
   } catch (err) {
@@ -223,6 +235,7 @@ export async function fetchFilteredCustomers(query: string) {
 export async function getUser(email: string) {
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
+    noStore();
     return user.rows[0] as User;
   } catch (error) {
     console.error('Failed to fetch user:', error);
